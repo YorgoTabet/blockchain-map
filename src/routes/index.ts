6a66fbefbad994068e2f6de773127250f0ff8b03
yorgotabet <yorgo.t@eurisko.net>
2024-06-28 17:18:37 +0300

    fix(setup): fix absolute imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,11 @@ import { mapRoute } from "./map/index";
 import { rootRoute } from "./root";
 import { createRouter } from "@tanstack/react-router";
 
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router;
+    }
+}
+
 const routeTree = rootRoute.addChildren([mapRoute]);
 export const router = createRouter({ routeTree });
