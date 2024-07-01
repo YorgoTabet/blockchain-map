fb9a86e5a0422204139bbfa4939d9e5fc2a76010
yorgotabet <yorgo.t@eurisko.net>
2024-07-01 22:33:12 +0300

    feat(map): adjust map styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { RouterProvider } from "@tanstack/react-router";
 import "./App.css";
 import { router } from "routes";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 function App() {
     return <RouterProvider router={router} />;
 }
--- src/components/templates/AppWrapper/index.tsx
+++ src/components/templates/AppWrapper/index.tsx
@@ -1,17 +1,36 @@
+import { Box, Toolbar } from "@mui/material";
 import { Outlet } from "@tanstack/react-router";
 import React from "react";
 
 // TODO: ADD NAVIGATION HERE
 export const AppWrapper: React.FC = () => {
     return (
-        <>
-            {/* <p style={{ position: "absolute", top: 0, height: "fit-content" }}> */}
-            Puffsterz
-            {/* </p> */}
-            <hr />
-            <div style={{ border: "1px solid red", flexGrow: 1 }}>
+        <Box
+            sx={{
+                m: 0,
+                p: 0,
+                display: "flex",
+                flexDirection: "column",
+                height: "100vh"
+            }}>
+            <Toolbar
+                style={{
+                    height: "fit-content",
+                    backgroundColor: "black",
+                    zIndex: 1000
+                }}>
+                Puffsterz
+            </Toolbar>
+            <Box
+                style={{
+                    flexGrow: 1,
+                    width: "100%",
+                    margin: 0,
+                    padding: 0,
+                    border: "1px solid red"
+                }}>
                 <Outlet />
-            </div>
-        </>
+            </Box>
+        </Box>
     );
 };
